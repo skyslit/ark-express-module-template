430164b3ba8d5b09a4b016882409a54b7fa8e389
anandhu-dev <anandhu@skyslit.com>
2020-10-13 15:18:56 +0530

    Express module optimised

--- src/module.ts
+++ src/module.ts
@@ -1,34 +1,9 @@
 import { ArkExpressModule } from "@skyslit/ark-express";
-import TodoSchema from "./schema/Todo.schema";
-import handlers from "./handlers";
 
 export class AuthExpressModule extends ArkExpressModule {
   constructor() {
     super();
-    this.registerModel("todo", TodoSchema);
   }
 
-  main() {
-    /**
-     * @api {post} /todo Create Todo Item
-     * @apiName PostTodo
-     * @apiGroup Todo
-     *
-     * @apiParam (Request Body) {String} title Title of Todo Item.
-     *
-     * @apiExample Example usage:
-     *     body:
-     *     {
-     *       "title": "Pickup Groceries"
-     *     }
-     */
-    this.router.post("/todo", handlers(this).addTodo);
-    /**
-     * @api {get} /todo See all Todo Items
-     * @apiName GetAllTodo
-     * @apiGroup Todo
-     *
-     */
-    this.router.get("/todo", handlers(this).getTodo);
-  }
+  main() {}
 }
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-
-export type TodoItem = {
-  title: string;
-};
-
-export type TodoItemDocument = TodoItem & Document;
